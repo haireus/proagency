7bdd741e84776e5e38f16c09b4f3aaec4802d00f
Alexdinh07 <hai.dinh@infinityforce.com>
2022-12-20 17:28:58 +0700

    Fix: logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,20 @@
 'use strict';
 
-
-
 /**
  * navbar toggle
  */
 
-const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-const navbar = document.querySelector("[data-navbar]");
-const navCloseBtn = document.querySelector("[data-nav-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector('[data-nav-open-btn]');
+const navbar = document.querySelector('[data-navbar]');
+const navCloseBtn = document.querySelector('[data-nav-close-btn]');
+const overlay = document.querySelector('[data-overlay]');
 
 const elemArr = [navCloseBtn, overlay, navOpenBtn];
 
 for (let i = 0; i < elemArr.length; i++) {
-  elemArr[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
+  elemArr[i].addEventListener('click', function () {
+    navbar.classList.toggle('active');
+    overlay.classList.toggle('active');
   });
 }
 
@@ -24,33 +22,29 @@ for (let i = 0; i < elemArr.length; i++) {
  * toggle navbar & overlay when click any navbar-link
  */
 
-const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+const navbarLinks = document.querySelectorAll('[data-navbar-link]');
 
 for (let i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
+  navbarLinks[i].addEventListener('click', function () {
+    navbar.classList.toggle('active');
+    overlay.classList.toggle('active');
   });
 }
-
-
-
-
 
 /**
  * header & go-top-btn active
  * when window scroll down to 400px
  */
 
-const header = document.querySelector("[data-header]");
-const goTopBtn = document.querySelector("[data-go-top]");
+// const header = document.querySelector("[data-header]");
+// const goTopBtn = document.querySelector("[data-go-top]");
 
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 400) {
-    header.classList.add("active");
-    goTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    goTopBtn.classList.remove("active");
-  }
-});
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY >= 400) {
+//     header.classList.add("active");
+//     goTopBtn.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//     goTopBtn.classList.remove("active");
+//   }
+// });
