2c1ba5f7a28509591d6d020a55e3b9b6d25808b3
Alexdinh07 <hai.dinh@infinityforce.com>
2023-02-18 18:38:47 +0700

    fix: header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-/**
- * navbar toggle
- */
-
 const navOpenBtn = document.querySelector('[data-nav-open-btn]');
 const navbar = document.querySelector('[data-navbar]');
 const navCloseBtn = document.querySelector('[data-nav-close-btn]');
@@ -36,15 +32,15 @@ for (let i = 0; i < navbarLinks.length; i++) {
  * when window scroll down to 400px
  */
 
-// const header = document.querySelector("[data-header]");
-// const goTopBtn = document.querySelector("[data-go-top]");
+const header = document.querySelector('[data-header]');
+const goTopBtn = document.querySelector('[data-go-top]');
 
-// window.addEventListener("scroll", function () {
-//   if (window.scrollY >= 400) {
-//     header.classList.add("active");
-//     goTopBtn.classList.add("active");
-//   } else {
-//     header.classList.remove("active");
-//     goTopBtn.classList.remove("active");
-//   }
-// });
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 400) {
+    header.classList.add('active');
+    goTopBtn.classList.add('active');
+  } else {
+    header.classList.remove('active');
+    goTopBtn.classList.remove('active');
+  }
+});
