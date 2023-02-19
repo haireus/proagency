c4000e105c0639a2a6c9390c3f2dd0b8fa8ffdfb
Alexdinh07 <hai.dinh@infinityforce.com>
2023-02-19 10:11:44 +0700

    fix: main feature

---
+++ assets/js/swiper.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 30,
+    },
+  },
+});
