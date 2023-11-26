043826a6e04bee75bb86cc27de0a37e19efcd7cf
buivandung97 <buivietdung9@yahoo.com>
2023-11-27 01:33:26 +0700

    update multi language

---
+++ assets/js/language.js
@@ -0,0 +1,287 @@
+var arrLang = {
+    "en-gb": {
+      "HOME": "Home",
+      "ACCOUNT": "About Us",
+      "PROCESS": "Process",
+      "QUESTION": "Question",
+      "CONTACT": "Contact",
+      "TITLE1A": "Rental Facebook",
+      "TITLE1B": "AD Accounts",
+      "TITLE1C": "Services",
+      "DESC1": "Whether you are looking to enhance your brand’s social media presence or drive targeted leads through social media advertising, our social media advertising services can help you hit your goals.",
+      "START": "Start now",
+      "TITLE2": "Facebook Account",
+      "DESC2": "Facebook is gradually changing and it is more difficult and  affects each business's Advertising. We have many years of advertising experience and have many ways to overcome all difficulties when customers advertise.",
+      "STATUS1": "All Product",
+      "DESC-STATUS1": "Allow all White hat, light Grey hat products.",
+      "STATUS2": "Refund",
+      "DESC-STATUS2": "We will refund amount left when you want stop work with our ads account.",
+      "STATUS3": "VAT",
+      "DESC-STATUS3": "ZERO %VAT.",
+      "STATUS4": "Unlimited",
+      "DESC-STATUS4": "Unlimited spending right after initialization.",
+      "STATUS5": "Security",
+      "DESC-STATUS5": "Campaign security, quick top up.",
+      "STATUS6": "Contract",
+      "DESC-STATUS6": "Free fanpage appeal, have business contract.",
+      "TITLE3": "My services include",
+      "TAB1": "Personal Ads account",
+      "TAB2": "Business Manager",
+      "TAB3": "Fanpage",
+      "TAB4": "Profile",
+      //TAB1
+      "TAB1-TITLE-NEW": "Personal account (new)",
+      "TAB1-TITLE-OLD": "Personal account (old)",
+      "LIMIT-50": "Limit $50",
+      "LIMIT-250": "Limit $250",
+      "LIMIT-1500": "Limit $1500",
+      "LIMIT-NO": "No limit",
+      "DESC1-LIMIT-50-250": "Never used before, with the ability to change time zone and currency.",
+      "DESC2-LIMIT-50": "Can spend up to $50 per day.",
+      "DESC2-LIMIT-250": "Can spend up to $250 per day.",
+      "DESC3-LIMIT-50-250": "Regular spending can increase the limit.",
+      "DESC2-LIMIT-1500": "Can spend up to $1500 per day.",
+      "DESC2-LIMIT-NO": "Can spend up to $5000 to Unlimited per day.",
+      "DESC-TITLE-OLD": "Used for previous White hat campaigns with many successful payouts, high reliability and good health.",
+      "DESC1-NOLIMIT-OLD": "Can spend from $5000 to unlimited per day.",
+      //TAB2
+      "BM-NORMAL": "Business Manager (normal)",
+      "BM-VERIFY": "Business Manager (verified)",
+      "DESC-BM-NORMAL-1": "Created with reputable profiles, good health, long life.",
+      "DESC-BM-NORMAL-3": "Regular spending can increase the limit.",
+      "DESC-BM-50": "Can spend up to $50 per day.",
+      "DESC-BM-250": "Can spend up to $250 per day.",
+      "DESC-BM-1500": "Can spend up to $1500 per day.",
+      "DESC-BM-NORMAL-NOLIMIT": "Can spend from $5000 to unlimited per day.",
+      "DESC-BM-OLD-1": 'Verified with Facebook and has a "blue mark" , extremely good health, and is great to use for long-term White hat and Black hat campaigns.',
+      "DESC-BM-OLD-NOLIMIT": "Can spend from $5000 to unlimited per day.",
+      //TAB3
+      "PAGE-NEW": "New fanpage",
+      "PAGE-OLD": "Old fanpage",
+      "PAGE-VERIFY": "Verified fanpage",
+      "DESC-NEW": "Created on reputable profiles, can be used immediately and can be renamed, no followers",
+      "DESC-OLD": "Created a long time ago and has long-standing posts, has a real following.",
+      "DESC-VERIFY": 'Verified with Facebook and has a "blue mark", extremely good health.',
+      //TAB4
+      "DESC-PROFILE": "Crafted and cared for by us, in good health, verified for instant advertising use.",
+      //PROCESS
+      "TITLE-PROCESS": "PROCESS",
+      "DESC-PROCESS": "My services include all thing for run ads : VPS or adspower (clean proxy), strong profile to keep ads account, strong ads account for run ads, Credit card avail in ads acc, we provide page too if you need. you just need care about performance of your ads.",
+      "PROCESS-STEP-1-TITLE": "Test your ads",
+      "PROCESS-STEP-1-DESC": "I will test your ads in my ads acc to make sure your ads work in my ads acc or not.",
+      "PROCESS-STEP-2-TITLE": "Prepare Ads Account",
+      "PROCESS-STEP-2-DESC": "If your ads work in my ads acc. I will prepare ads acc and show you to check my ads acc.",
+      "PROCESS-STEP-3-TITLE": "Payment",
+      "PROCESS-STEP-3-DESC": "If your ads work in my ads acc. I will prepare ads acc and show you to check my ads acc.",
+      "PROCESS-STEP-4-TITLE": "Note",
+      "PROCESS-STEP-4-DESC": "Campaigns will be checked first to save time for both parties. You will also be able to check our account first, just in case someone impersonates us for SCAM.",
+      //OUR FEATURES
+      "FEATURE-TITLE": "Our Features",
+      "F-1-TITLE": "Invoice Trusted Ads Accounts",
+      "F-1-DESC": "Accounts are provided directly by authorized agency, with high trust factor, speediest ads approval time and easiest review progress for hard ads like DROP, P.O.D, comestics...",
+      "F-2-TITLE": "Fast, Low and Transparent Fee",
+      "F-2-DESC": "247 same day top up with low and transparent fee. Support many kinds of payment method.",
+      "F-3-TITLE": "Total Ad Control",
+      "F-3-DESC": "Ads accounts will be shared to you, you can check anytime and will in total control of ads, cost, and customer data!",
+      "F-4-TITLE": "Support & Consultancy",
+      "F-4-DESC": "Our services include full technical supports from 10 years experienced expert on FB ads for helping you set up a running system and create ads following FB policies.",
+      //QUESTION
+      "QUESTION-TITLE": "Frequently Asked Questions",
+      "Q-1": "Can your account run Black Hat and Gray Hat campaigns?",
+      "A-1": "Yes, our account can be used for Black Hat and Grey Hat campaigns.",
+
+      "Q-2": "What types of accounts do you provide?",
+      "A-2": "We provide all account types: Personal accounts with spending history, BM accounts and agency accounts.",
+
+      "Q-3": "What happens when the account is locked? Can the money in that account be transferred to the new account?",
+      "A-3": "When the account is locked, our staff will provide you with a new account completely free of charge.",
+      "A-3A": "The balance in the old account will be transferred to the new account. This applies to both profile accounts and page accounts.",
+
+      "Q-4": "What is your account limit??",
+      "A-4": "Our account has spending limits of $1500, 250 and no limit daily.",
+
+      "Q-5": "How do I use your account?",
+      "A-5": "We set up everything and ad accounts in VPS, RDP or Adspower.",
+      "A-5A": "You will be granted access to these software to control and use our account.",
+
+      "Q-6": "Which payment method do you use?",
+      "A-6": "We use USDT ,BTC, Payoneer for payment.",
+
+      "Q-7": "How to top up your account?",
+      "A-7": "You send us money after checking our account. Then our staff will activate the account for you to use.",
+      "A-7A": "When your deposit has almost been spent, our staff will notify you to deposit more so that your campaign will not be interrupted.",
+
+      "Q-8": "What is your refund process?",
+      "A-8": "When you want to stop cooperation. Your support staff will stop the campaign and aggregate your spending, calculate the balance and confirm the balance with you.",
+      "A-8A": "If there are no problems, the request will be forwarded to the accounting department for completion. This process will take 1-2 days.",
+
+      "Q-9": "What is the service fee?",
+      "A-9": "We charge a service fee. This fee is 8-12% for White hat campaigns and 10-12% for Black hat campaigns.",
+      "A-9A": "This fee may change if you spend well and stably.",
+
+      "Q-10": "What is the minimum spend to use your service?",
+      "A-10": "$100 is the minimum spent on our account.",
+      "A-10A": "You can test campaigns with a smaller budget, but you need to make sure to scale them after testing.",
+      //CONTACT
+      "CONTACT-TITLE": "Contact us",
+      "CONTACT-DESC": "Contact us immediately to receive special offers and thoughtful support",
+
+    },
+    "zh-tw": {
+      "HOME": "主页",
+      "ACCOUNT": "帐户",
+      "PROCESS": "过程",
+      "QUESTION": "问题",
+      "CONTACT": "接触",
+      "TITLE1A": "出租脸书",
+      "TITLE1B": "广告账户",
+      "TITLE1C": "服务",
+      "DESC1": "无论您是希望增强品牌的社交媒体影响力，还是通过社交媒体广告吸引有针对性的潜在客户，我们的社交媒体广告服务都可以帮助您实现目标。",
+      "START": "现在开始",
+      "TITLE2": "脸书账号",
+      "DESC2": "Facebook正在逐渐发生变化，变得更加困难，并影响到每个企业的广告。 我们有多年的广告经验，有很多方法可以克服客户做广告时的一切困难。",
+      "STATUS1": "所有产品",
+      "DESC-STATUS1": "允许所有白帽、浅灰帽产品。",
+      "STATUS2": "退款",
+      "DESC-STATUS2": "当您想停止使用我们的广告帐户时，我们将退还剩余金额。",
+      "STATUS3": "增值税",
+      "DESC-STATUS3": "零％增值税。",
+      "STATUS4": "无限",
+      "DESC-STATUS4": "初始化后即可无限消费。",
+      "STATUS5": "安全",
+      "DESC-STATUS5": "活动安全，充值快捷。",
+      "STATUS6": "合同",
+      "DESC-STATUS6": "免费粉丝专页呼吁，有商业合同。",
+      "TAB1": "个人广告帐户",
+      "TAB2": "业务经理",
+      "TAB3": "粉丝专页",
+      "TAB4": "个人资料",
+      //TAB1
+      "TAB1-TITLE-NEW": "个人帐户（新）",
+      "TAB1-TITLE-OLD": "个人帐户（旧）",
+      "LIMIT-50": "限额 50 美元",
+      "LIMIT-250": "限额 250 美元",
+      "LIMIT-1500": "限额 1500 美元",
+      "LIMIT-NO": "没有限制",
+      "DESC1-LIMIT-50-250": "以前从未使用过，具有更改时区和货币的能力。",
+      "DESC2-LIMIT-50": "每天最多可以花费 50 美元。",
+      "DESC2-LIMIT-250": "每天最多可以花费 250 美元。",
+      "DESC3-LIMIT-50-250": "定期消费可以提高限额。",
+      "DESC2-LIMIT-1500": "每天最多可以花费 1500 美元。",
+      "DESC2-LIMIT-NO": "每天最多可以花费 5000 美元至无限制。",
+      "DESC-TITLE-OLD": "用于之前的白帽活动，具有许多成功的支付、高可靠性和良好的健康状况。",
+      "DESC1-NOLIMIT-OLD": "每天可以消费 5000 美元到无限制。",
+      //TAB2
+      "BM-NORMAL": "业务经理（普通）",
+      "BM-VERIFY": "业务经理（已验证）",
+      "DESC-BM-NORMAL-1": "以信誉良好的形象创建，身体健康，寿命长。",
+      "DESC-BM-NORMAL-3": "定期消费可以提高限额。",
+      "DESC-BM-50": "每天最多可以花费 50 美元。",
+      "DESC-BM-250": "每天最多可以花费 250 美元。",
+      "DESC-BM-1500": "每天最多可以花费 1500 美元。",
+      "DESC-BM-NORMAL-NOLIMIT": "每天可以花费 5000 美元到无限制。",
+      "DESC-BM-OLD-1": '已通过 Facebook 验证并具有“蓝色标记”，非常健康，非常适合长期白帽和黑帽活动。',
+      "DESC-BM-OLD-NOLIMIT": "每天可以花费 5000 美元到无限制。",
+      //TAB3
+      "PAGE-NEW": "新粉丝专页",
+      "PAGE-OLD": "旧粉丝专页",
+      "PAGE-VERIFY": "已验证粉丝专页",
+      "DESC-NEW": "在信誉良好的个人资料上创建，可以立即使用，可以重命名，没有关注者",
+      "DESC-OLD": "创建很久以前，有长期的帖子，有真正的追随者。",
+      "DESC-VERIFY": '已通过 Facebook 验证并具有“蓝色标记”，非常健康。',
+      //TAB4
+      "DESC-PROFILE": "由我们精心制作和保养，健康状况良好，经过验证可用于即时广告用途。",
+      //PROCESS
+      "TITLE-PROCESS": "过程",
+      "DESC-PROCESS": "我的服务包括运行广告的所有内容：VPS 或 adspower（干净的代理）、保留广告帐户的强大配置文件、用于运行广告的强大广告帐户、广告帐户中的信用卡可用性，我们也提供页面，如果 您需要。您只需要关心广告的效果。",
+      "PROCESS-STEP-1-TITLE": "测试您的广告",
+      "PROCESS-STEP-1-DESC": "我将在我的广告 acc 中测试您的广告，以确保您的广告在我的广告 acc 中正常运行。",
+      "PROCESS-STEP-2-TITLE": "准备广告帐户",
+      "PROCESS-STEP-2-DESC": "如果您的广告在我的广告帐户中有效，我将准备广告帐户并显示您检查我的广告帐户。",
+      "PROCESS-STEP-3-TITLE": "付款",
+      "PROCESS-STEP-3-DESC": "如果您的广告在我的广告帐户中有效，我将准备广告帐户并显示您检查我的广告帐户。",
+      "PROCESS-STEP-4-TITLE": "注意",
+      "PROCESS-STEP-4-DESC": "将首先检查活动，以节省双方时间。您还可以先检查我们的帐户，以防有人冒充我们进行诈骗。",
+      //OUR FEATURES
+      "FEATURE-TITLE": "我们的特色",
+      "F-1-TITLE": "为可信广告帐户开具发票",
+      "F-1-DESC": "账号由授权机构直接提供，信任度高，广告审批时间最快，审核进度最简单，对于DROP、POD、美妆等硬广告...",
+      "F-2-TITLE": "快速、低廉且透明的费用",
+      "F-2-DESC": "247当日充值，费用低且透明。支持多种支付方式。",
+      "F-3-TITLE": "全面广告控制",
+      "F-3-DESC": "广告账户将共享给您，您可以随时查看，并完全掌控广告、费用和客户数据！",
+      "F-4-TITLE": "支持与咨询",
+      "F-4-DESC": "我们的服务包括由 10 年 FB 广告经验丰富的专家提供的全面技术支持，帮助您建立运行系统并按照 FB 政策创建广告。",
+      //QUESTION
+      "QUESTION-TITLE": "常见问题",
+      "Q-1": "您的帐户可以运行黑帽和灰帽活动吗？",
+      "A-1": "是的，我们的帐户可用于黑帽和灰帽活动。",
+
+      "Q-2": "你们提供什么类型的账户？",
+      "A-2": "我们提供所有账户类型：有消费历史的个人账户、BM账户和代理账户。",
+
+      "Q-3": "账户被锁定后会怎样？那个账户里的钱可以转到新账户吗？",
+      "A-3": "当账户被锁定时，我们的工作人员将完全免费为您提供一个新账户。",
+      "A-3A": "旧帐户中的余额将转移到新帐户。这适用于个人资料帐户和页面帐户。",
+
+      "Q-4": "您的账户限额是多少？？",
+      "A-4": "我们的账户支出限额为 1500 美元、250 美元，每日无限额。",
+
+      "Q-5": "我如何使用你们的帐户？",
+      "A-5": "我们在 VPS、RDP 或 Adspower 中设置了所有内容和广告帐户。",
+      "A-5A": "您将被授予访问这些软件的权限来控制和使用我们的帐户。",
+
+      "Q-6": "您使用哪种付款方式？",
+      "A-6": "我们使用USDT、BTC、Payoneer进行支付。",
+
+      "Q-7": "如何给账户充值？",
+      "A-7": "您查看我们的账户后给我们汇款，然后我们的工作人员会激活账户供您使用。",
+      "A-7A": "当您的押金快用完时，我们的工作人员会通知您补充押金，以免您的活动中断。",
+
+      "Q-8": "你们的退款流程是怎样的？",
+      "A-8": "当您想停止合作时。您的支持人员将停止活动并汇总您的支出，计算余额并与您确认余额。",
+      "A-8A": "如果没有问题，请求将转发给会计部门完成。这个过程需要1-2天。",
+
+      "Q-9": "服务费是多少？",
+      "A-9": "我们收取服务费。此费用对于白帽活动为 8-12%，对于黑帽活动为 10-12%。",
+      "A-9A": "如果您消费良好且稳定，此费用可能会发生变化。",
+
+      "Q-10": "使用你们服务的最低消费是多少？",
+      "A-10": "我们账户的最低消费额为 100 美元。",
+      "A-10A": "您可以使用较小的预算来测试广告系列，但需要确保在测试后对其进行扩展。",
+      //CONTACT
+      "CONTACT-TITLE": "联系我们",
+      "CONTACT-DESC": "立即联系我们以获得特别优惠和周到的支持",
+
+    }
+  };
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // The default language is English
+    var lang = localStorage.getItem("language");
+    if(!lang || lang == null){
+      var lang = "en-gb";
+    }
+    
+    // Function to update language for elements with the class "lang"
+    function updateLanguage() {
+        var elements = document.querySelectorAll(".lang");
+        elements.forEach(function(element) {
+            var key = element.getAttribute("key");
+            element.textContent = arrLang[lang][key];
+        });
+    }
+
+    // Set default language on page load
+    updateLanguage();
+
+    // Event listener for click on elements with the class "translate"
+    var translateButtons = document.querySelectorAll(".translate");
+    translateButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            localStorage.setItem("language", this.id);
+            lang = this.id;
+            updateLanguage();
+        });
+    });
+});
