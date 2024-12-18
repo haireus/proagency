28de618f7d77aaf43f46a93a3f17e28f3ee91cd6
buivandung97 <buivietdung9@yahoo.com>
2024-12-18 10:40:40 +0700

    updated mail

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,20 @@ for (let i = 0; i < navbarLinks.length; i++) {
 //     goTopBtn.classList.remove('active');
 //   }
 // });
+function myFunctionMail() {
+  // Get the text field
+  var copyText = document.getElementById("mail-main");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  // alert("Copied the text: " + copyText.value);
+}
 
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
