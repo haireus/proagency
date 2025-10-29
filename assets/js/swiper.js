8666bcac322223bdcbc3b80c47460e341493c80c
Alex <alex@jetweels.com>
2025-10-29 13:11:35 +0700

    fix: aaa

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,28 +1,42 @@
 const swiper = new Swiper('.mySwiper', {
+  loop: true,
+  centeredSlides: false,
+  grabCursor: true,
+
   pagination: {
     el: '.swiper-pagination',
-    dynamicBullets: true,
     clickable: true,
+    dynamicBullets: true,
   },
+
   autoplay: {
-    delay: 2000,
+    delay: 500,
     disableOnInteraction: false,
+    pauseOnMouseEnter: false,
   },
 
   breakpoints: {
-    300: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    480: {
       slidesPerView: 2,
       spaceBetween: 20,
     },
     640: {
-      slidesPerView: 2,
+      slidesPerView: 3,
       spaceBetween: 20,
     },
     768: {
       slidesPerView: 4,
-      spaceBetween: 40,
+      spaceBetween: 25,
     },
     1024: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+    1280: {
       slidesPerView: 6,
       spaceBetween: 30,
     },
